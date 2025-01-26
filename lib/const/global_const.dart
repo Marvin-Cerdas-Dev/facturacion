e1229fafc27d42b125e473547ece8ef1fd24bdec
Marvin Cerdas <marvin.cerdas.dev@gmail.com>
2025-01-25 21:16:43 -0600

    Update global_const.dart
    Update

--- lib/const/global_const.dart
+++ lib/const/global_const.dart
@@ -3,7 +3,7 @@ class GlobalConst {
 
   static const String USER_NAME = "sandbox@factus.com.co";
   static const String PASSWORD = "sandbox2024%";
-  static const String CLIENT_ID = "9df3af30-4963-4df5-8bc7-5ca561a566ba";
+  static const String CLIENT_ID = "9df3af30-4963-4df5-8bc7-5ca56aeiqaaserAasdas";
   static const String CLIENTE_SECRET =
-      "He4fD7sz5E59SR3v5d3WiOYPDl1SEZFGKoEH89e9";
+      "He4fD7sz5E59SR3v5d3WiOYPDl1SEZFGAsasd09hjsda09ahj0990";
 }
